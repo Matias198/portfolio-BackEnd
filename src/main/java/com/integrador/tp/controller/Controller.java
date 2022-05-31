272ace47ddf998ff4b57f74e73c40e6346ea174d
Matias198 <matias.fernandez099@gmail.com>
2022-05-30 23:30:04 -0300

    v1.5

--- src/main/java/com/integrador/tp/controller/Controller.java
+++ src/main/java/com/integrador/tp/controller/Controller.java
@@ -19,10 +19,7 @@ import com.integrador.tp.service.ISeccionService;
 import com.integrador.tp.service.ISkillService;
 import com.integrador.tp.service.IUsuarioService;
 import java.net.URISyntaxException;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -70,12 +67,23 @@ public class Controller {
         }
     }
     
+    @PostMapping ("/borrar-academico")
+    public void borrarAcademico(@RequestParam Long id) throws URISyntaxException {
+        aServ.borrarAcademico(id);   
+    }
+    
     @PostMapping ("/crear-compania")
     public void crearCompania(@ModelAttribute Compania CompaniaDTO, @RequestParam Long dni) throws URISyntaxException {
         if (peServ.buscarPersona(dni) != null){
             CompaniaDTO.setPersona(peServ.buscarPersona(dni));
             cServ.crearCompania(CompaniaDTO);
         }
+    }
+    
+    @PostMapping ("/borrar-compania")
+    public void borrarCompania(@RequestParam Long id) throws URISyntaxException {
+        cServ.borrarCompania(id);
+        
     }
     
     @PostMapping ("/crear-escuela")
@@ -86,12 +94,24 @@ public class Controller {
         }
     }
     
+    @PostMapping ("/borrar-escuela")
+    public void borrarEscuela(@RequestParam Long id) throws URISyntaxException {
+        esServ.borrarEscuela(id);
+        
+    }
+    
     @PostMapping ("/crear-experiencia")
     public void crearExperiencia(@ModelAttribute Experiencia ExperienciaDTO, @RequestParam Long dni) throws URISyntaxException {
         if (peServ.buscarPersona(dni) != null){
             ExperienciaDTO.setPersona(peServ.buscarPersona(dni));
             exServ.crearExperiencia(ExperienciaDTO);
         }
+    }
+    
+    @PostMapping ("/borrar-experiencia")
+    public void borrarExperiencia(@RequestParam Long id) throws URISyntaxException {
+        exServ.borrarExperiencia(id);
+        
     }
     
     @PostMapping ("/crear-proyecto")
@@ -102,12 +122,22 @@ public class Controller {
         }
     }
     
+    @PostMapping ("/borrar-proyecto")
+    public void borrarProyecto(@RequestParam Long id) throws URISyntaxException {
+        prServ.borrarProyecto(id);
+    }
+    
     @PostMapping ("/crear-seccion")
     public void crearSeccion(@ModelAttribute Seccion SeccionDTO, @RequestParam Long dni) throws URISyntaxException {
         if (peServ.buscarPersona(dni) != null){
             SeccionDTO.setPersona(peServ.buscarPersona(dni));
             seServ.crearSeccion(SeccionDTO);
         }
+    }
+    
+    @PostMapping ("/borrar-seccion")
+    public void borrarSeccion(@RequestParam Long id) throws URISyntaxException {
+        seServ.borrarSeccion(id);
     }
     
     @PostMapping ("/crear-skill")
@@ -117,7 +147,13 @@ public class Controller {
             skServ.crearSkill(SkillDTO);
         }
     }
+    
+    @PostMapping ("/borrar-skill")
+    public void borrarSkill(@RequestParam Long id) throws URISyntaxException {
+        skServ.borrarSkill(id);
         
+    }
+    
     @PostMapping ("/crear-persona")
     public void crearPersona(@ModelAttribute Persona PersonaDTO) throws URISyntaxException {
         peServ.crearPersona(PersonaDTO);
@@ -133,44 +169,46 @@ public class Controller {
         
     @PostMapping ("/iniciar-sesion")
     @ResponseBody
-    public Usuario searchUser(@ModelAttribute Usuario UsuarioDTO) throws URISyntaxException {
-        if (UsuarioDTO.getPassword().equals(uServ.buscarUsuario(UsuarioDTO.getId_usuario()).getPassword())){
-            //UsuarioDTO.setPersona(persoServ.buscarPersona(UsuarioDTO.getId_usuario())); !explota
-            return UsuarioDTO;
+    public Usuario searchUser(@RequestParam Long id_usuario, @RequestParam String password) throws URISyntaxException {
+        
+        //Obtengo la Persona del user
+        Persona perso = peServ.buscarPersona(id_usuario);
+        
+        //Obtener el Usuario del UsuarioDTO
+        Usuario user = perso.getUsuario();
+        
+        if (user.getId_usuario().equals(id_usuario)){
+            if (user.getPassword().equals(password)){
+                return user;
+            }else{
+                return null;
+            }
         }else{
             return null;
         }
+        
+        
+        
+
+//        UsuarioDTO.setId_usuario(Long.MIN_VALUE);
+//        Usuario user = uServ.buscarUsuario(UsuarioDTO.getId_usuario());
+//        if (user != null){
+//            Persona perso = user.getPersona();
+//            Long id_usuario = perso.getUsuario().getId_usuario();
+//            String password = perso.getUsuario().getPassword();
+//            if (UsuarioDTO.getId_usuario().equals(id_usuario) && UsuarioDTO.getPassword().equals(password)){
+//                return perso.getUsuario();
+//            }else{
+//                return null;
+//            }
+//        }else{
+//            return null;
+//        }
     }
     
     @GetMapping ("/portfolio")
     @ResponseBody
-    public ResponseEntity<Persona> getPersona(@RequestParam Long dni){
-        Persona demo = peServ.buscarPersona(dni);
-        Persona perso = new Persona();
-        perso.setDni(demo.getDni());
-        perso.setNombres(demo.getNombres());
-        perso.setApellido(demo.getApellido());
-        perso.setFecha_nacimiento(demo.getFecha_nacimiento());
-        perso.setNacionalidad(demo.getNacionalidad());
-        perso.setMail(demo.getMail());
-        perso.setOcupacion(demo.getOcupacion());
-        perso.setImage_background(demo.getImage_background());
-        perso.setImage_perfil(demo.getImage_perfil());
-        
-//        List<Experiencia> listaExperiencia;
-//        listaExperiencia = demo.getExperiencias();
-//        perso.setExperiencias(listaExperiencia);
-        return new  ResponseEntity<>(demo, HttpStatus.OK);
-    }
-    
-    @GetMapping ("/ver-experiencias")
-    @ResponseBody
-    public Persona getExperiencia(@RequestParam Long dni){
-//        Persona aux = peServ.buscarPersona(dni);
-//        List<Experiencia> listaExperiencias;
-//        listaExperiencias = aux.getExperiencias();
-//        listaExperiencias.forEach(System.out::println);
-        
+    public Persona getPersona(@RequestParam Long dni){
         return peServ.buscarPersona(dni);
     }
 }
@@ -187,5 +225,11 @@ spring.jpa.hibernate.ddl-auto=update
 spring.datasource.url = jdbc:mysql://sql10.freesqldatabase.com:3306/sql10493123?useSSL=false&serverTimezone=UTC
 spring.datasource.username=sql10493123
 spring.datasource.password=MZ6eFhy5jB
+spring.jpa.database-platform=org.hibernate.dialect.MySQL8Dialect
+
+spring.jpa.hibernate.ddl-auto=update
+spring.datasource.url = jdbc:mysql://bcyrnmnbhoaehisdzisw-mysql.services.clever-cloud.com:3306/bcyrnmnbhoaehisdzisw?useSSL=false&serverTimezone=UTC
+spring.datasource.username=uuh0dphvfr9uywnt
+spring.datasource.password=9FtmZX2D4aZ7yyQKu8bO
 spring.jpa.database-platform=org.hibernate.dialect.MySQL8Dialect
 */
