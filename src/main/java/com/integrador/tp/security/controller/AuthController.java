50c4f4ce5e19bc0d4617cb38a3ccb4ee0a82d760
Matias198 <matias.fernandez099@gmail.com>
2022-07-21 14:56:04 -0300

    v1.3

--- src/main/java/com/integrador/tp/security/controller/AuthController.java
+++ src/main/java/com/integrador/tp/security/controller/AuthController.java
@@ -29,11 +29,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class AuthController {
 
     @Autowired
