3306a681e84f069f69e2ef20bbe137b08ad7aee8
Matias198 <matias.fernandez099@gmail.com>
2022-07-21 16:00:06 -0300

    v1.3

--- src/main/java/com/integrador/tp/TpIntegradorApplication.java
+++ src/main/java/com/integrador/tp/TpIntegradorApplication.java
@@ -3,10 +3,14 @@ package com.integrador.tp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@Configuration
+@EnableWebSecurity
 public class TpIntegradorApplication {
 
     public static void main(String[] args) {
@@ -15,6 +19,7 @@ public class TpIntegradorApplication {
     }
 
     @Bean
+    
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
