f8a433314fe1e595e5f85838de72eeb3c310c3f2
Matias198 <matias.fernandez099@gmail.com>
2022-07-21 17:39:12 -0300

    v1.4

--- src/main/java/com/integrador/tp/WebConfig.java
+++ src/main/java/com/integrador/tp/WebConfig.java
@@ -1,19 +1,15 @@
-package com.integrador.tp.security;
+package com.integrador.tp;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
