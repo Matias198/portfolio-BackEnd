3aea3812afb9c0496b879099ecda49a0ace558ee
Matias198 <matias.fernandez099@gmail.com>
2022-07-21 14:47:41 -0300

    v1.3

--- src/main/java/com/integrador/tp/TpIntegradorApplication.java
+++ src/main/java/com/integrador/tp/TpIntegradorApplication.java
@@ -2,6 +2,8 @@ package com.integrador.tp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TpIntegradorApplication {
@@ -9,5 +11,14 @@ public class TpIntegradorApplication {
     public static void main(String[] args) {
         SpringApplication.run(TpIntegradorApplication.class, args);
         System.out.println("Backend iniciado");
+    }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://porfolio-matias-fernandez.web.app").allowedMethods("*").allowedHeaders("*");
+            }
+        };
     }
 }
