5bc3a4b6d7068b88ae7012d59ac393a961ac4eef
Matias198 <matias.fernandez099@gmail.com>
2023-05-29 20:05:40 -0300

    cambio de variables

--- src/main/java/com/integrador/tp/CorsConfiguration.java
+++ src/main/java/com/integrador/tp/CorsConfiguration.java
@@ -1,4 +1,3 @@
-
 package com.integrador.tp;
 
 import org.springframework.context.annotation.Bean;
@@ -7,11 +6,10 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration 
-{
+public class CorsConfiguration {
+
     @Bean
-    public WebMvcConfigurer corsConfigurer() 
-    {
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
